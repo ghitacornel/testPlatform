ee83bfbb23d82732e6b81bbd8425c3efee4f1435
cornel <ghita.cornel@gmail.com>
2022-10-01 20:58:26 +0300

    better

--- companies-platform/src/main/java/companies/service/mapper/CompanyMapper.java
+++ companies-platform/src/main/java/companies/service/mapper/CompanyMapper.java
@@ -2,17 +2,10 @@ package companies.service.mapper;
 
 import companies.controller.model.CompanyDto;
 import companies.repository.entity.Company;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class CompanyMapper {
-    public CompanyDto map(Company model) {
-        return CompanyDto.builder()
-                .id(model.getId())
-                .name(model.getName())
-                .url(model.getUrl())
-                .industry(model.getIndustry())
-                .build();
-    }
+@Mapper
+public interface CompanyMapper {
+    CompanyDto map(Company model);
 
 }
