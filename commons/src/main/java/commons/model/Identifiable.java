b779a4b36d4e5486e19b7d717c2515947bf280c7
cornel <ghita.cornel@gmail.com>
2022-10-01 21:02:55 +0300

    better

--- commons/src/main/java/commons/model/Identifiable.java
+++ commons/src/main/java/commons/model/Identifiable.java
@@ -2,9 +2,11 @@ package commons.model;
 
 import lombok.Getter;
 import lombok.ToString;
-import org.hibernate.Hibernate;
 
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.util.Objects;
 
 @MappedSuperclass
@@ -19,7 +21,7 @@ public abstract class Identifiable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        if (o == null || !this.getClass().equals(o.getClass())) return false;
         Identifiable that = (Identifiable) o;
         return id != null && Objects.equals(id, that.id);
     }
