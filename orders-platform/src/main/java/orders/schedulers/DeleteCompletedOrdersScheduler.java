15ebdf1b3bf67a7767c583cf9b2fb94a41406060
cornel <ghita.cornel@gmail.com>
2022-10-02 22:42:11 +0300

    better

--- orders-platform/src/main/java/orders/schedulers/DeleteCompletedOrdersScheduler.java
+++ orders-platform/src/main/java/orders/schedulers/DeleteCompletedOrdersScheduler.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import orders.repository.OrderRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -12,6 +14,7 @@ public class DeleteCompletedOrdersScheduler {
     private final OrderRepository orderRepository;
 
     @Scheduled(fixedRate = 1000)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void deleteCompletedOrders() {
         orderRepository.deleteCompletedOrders();
     }
