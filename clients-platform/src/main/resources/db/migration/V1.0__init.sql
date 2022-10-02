e4c788fc3b3983e5f5df5203b4bc4688aaf82a8e
cornel <ghita.cornel@gmail.com>
2022-10-03 00:08:36 +0300

    better

--- clients-platform/src/main/resources/db/migration/V1.0__init.sql
+++ clients-platform/src/main/resources/db/migration/V1.0__init.sql
@@ -3,5 +3,5 @@ create table client
     id        int primary key GENERATED ALWAYS AS IDENTITY,
     name      varchar(50) not null unique,
     card_type varchar(50) not null,
-    status    int         not null,
+    status    int         not null
 );
