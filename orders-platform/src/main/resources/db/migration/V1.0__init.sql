f05c1fdc3b26de385aaadf14227d9a4dd1412674
cornel <ghita.cornel@gmail.com>
2022-12-07 21:12:12 +0200

    better

--- orders-platform/src/main/resources/db/migration/V1.0__init.sql
+++ orders-platform/src/main/resources/db/migration/V1.0__init.sql
@@ -2,14 +2,16 @@ create table T_ORDER
 (
     id                    int primary key GENERATED ALWAYS AS IDENTITY,
     user_id               int              not null,
-    user_name             varchar(255)      not null,
-    user_credit_card_type varchar(255)      not null,
+    user_name             varchar(255)     not null,
+    user_credit_card_type varchar(255)     not null,
+    user_country          varchar(255)     not null,
     product_id            int              not null,
-    product_name          varchar(255)      not null,
-    product_color         varchar(255)      not null,
+    product_name          varchar(255)     not null,
+    product_color         varchar(255)     not null,
     price                 double precision not null,
     quantity              int              not null,
     company_id            int              not null,
-    company_name          varchar(255)      not null,
+    company_name          varchar(255)     not null,
+    company_country       varchar(255)     not null,
     status                int              not null
 );
