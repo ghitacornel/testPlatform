2301a59e987815d86bdc1db20f7cda28792a5a6e
ghita <ghita.cornel@gmail.com>
2023-06-30 09:18:01 +0300

    better

--- clients-sim/src/main/java/client/ClientsSim.java
+++ clients-sim/src/main/java/client/ClientsSim.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClientsSimulator {
+public class ClientsSim {
     public static void main(String[] args) {
-        SpringApplication.run(ClientsSimulator.class, args);
+        SpringApplication.run(ClientsSim.class, args);
     }
 }
