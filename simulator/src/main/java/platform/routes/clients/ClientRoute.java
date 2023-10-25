86203ae63944b3ea68e471acdf5013b44f09fe0e
ghita <ghita.cornel@gmail.com>
2023-10-25 10:54:45 +0300

    better

---
+++ simulator/src/main/java/platform/routes/clients/ClientRoute.java
@@ -0,0 +1,19 @@
+package platform.routes.clients;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ClientRoute extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        from("timer://simpleTimer?period=5000")
+                .routeId("register-client-route")
+                .log("register new client ${body}")
+                .end();
+    }
+}
+
