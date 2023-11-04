6480501b62fbeecabce710f77371b097a2e2057c
ghita <ghita.cornel@gmail.com>
2023-11-04 23:20:27 +0200

    better

--- simulator/src/main/java/platform/feign/order/OrderContract.java
+++ simulator/src/main/java/platform/feign/order/OrderContract.java
@@ -1,7 +1,5 @@
 package platform.feign.order;
 
-import commons.model.IdResponse;
-import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 
@@ -9,21 +7,8 @@ import java.util.List;
 
 public interface OrderContract {
 
-    @RequestLine("GET /order/count")
-    long countAllNew();
-
     @RequestLine("GET /order")
     List<OrderDetailsResponse> findAllNew();
-
-    @RequestLine("GET /order/{id}")
-    OrderDetailsResponse findById(@Param("id") Integer id);
-
-    @RequestLine("POST /order")
-    @Headers("Content-Type: application/json")
-    IdResponse create(CreateOrderRequest inputModel);
-
-    @RequestLine("PATCH /order/complete/{id}")
-    void completeById(@Param("id") Integer id);
 
     @RequestLine("PATCH /order/cancel/{id}")
     void cancelById(@Param("id") Integer id);
