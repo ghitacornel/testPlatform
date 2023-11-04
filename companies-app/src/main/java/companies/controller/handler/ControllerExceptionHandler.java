15cbab19a55e7b177dc5c1e00f7beffff3919934
ghita <ghita.cornel@gmail.com>
2023-11-04 15:55:07 +0200

    better

--- companies-app/src/main/java/companies/controller/handler/ControllerExceptionHandler.java
+++ companies-app/src/main/java/companies/controller/handler/ControllerExceptionHandler.java
@@ -3,7 +3,6 @@ package companies.controller.handler;
 import commons.exceptions.CommonControllerExceptionHandler;
 import commons.exceptions.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
