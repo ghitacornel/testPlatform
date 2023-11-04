25f8dfea4b176a6500bcb2dc24bb45b9356bb098
ghita <ghita.cornel@gmail.com>
2023-11-04 20:26:54 +0200

    better

---
+++ business-flows/src/main/java/flows/BusinessFlowsApplication.java
@@ -0,0 +1,11 @@
+package flows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessFlowsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessFlowsApplication.class, args);
+    }
+}
