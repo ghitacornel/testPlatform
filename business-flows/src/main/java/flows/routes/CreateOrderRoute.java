e5d9a252d9aaaf50a0bafb451bdd9be18ea2964c
ghita <ghita.cornel@gmail.com>
2023-11-04 23:07:17 +0200

    better

--- business-flows/src/main/java/flows/routes/CreateOrderRoute.java
+++ business-flows/src/main/java/flows/routes/CreateOrderRoute.java
@@ -6,7 +6,6 @@ import flows.feign.order.OrderContract;
 import flows.feign.product.ProductBuyRequest;
 import flows.feign.product.ProductContract;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
