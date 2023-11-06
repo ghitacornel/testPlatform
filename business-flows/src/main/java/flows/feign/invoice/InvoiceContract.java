4ade783b05a8d6eab013a2bd96a131b53dee4d58
ghita <ghita.cornel@gmail.com>
2023-11-06 13:29:11 +0200

    better

--- business-flows/src/main/java/flows/feign/invoice/InvoiceContract.java
+++ business-flows/src/main/java/flows/feign/invoice/InvoiceContract.java
@@ -1,19 +1,24 @@
 package flows.feign.invoice;
 
+import feign.Headers;
 import feign.RequestLine;
 
 public interface InvoiceContract {
 
     @RequestLine("PATCH /invoice/update/order")
+    @Headers("Content-Type: application/json")
     void update(UpdateOrderRequest request);
 
     @RequestLine("PATCH /invoice/update/client")
+    @Headers("Content-Type: application/json")
     void update(UpdateClientRequest request);
 
     @RequestLine("PATCH /invoice/update/company")
+    @Headers("Content-Type: application/json")
     void update(UpdateCompanyRequest request);
 
     @RequestLine("PATCH /invoice/update/product")
+    @Headers("Content-Type: application/json")
     void update(UpdateProductRequest request);
 
 }
