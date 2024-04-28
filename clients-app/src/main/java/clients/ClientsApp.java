702289f29cee86ef69cfba5a3ff39257ed6dd7d8
ghita <ghita.cornel@gmail.com>
2024-04-28 20:14:10 +0300

    better

--- clients-app/src/main/java/clients/ClientsApp.java
+++ clients-app/src/main/java/clients/ClientsApp.java
@@ -2,10 +2,8 @@ package clients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 public class ClientsApp {
     public static void main(String[] args) {
         SpringApplication.run(ClientsApp.class, args);
