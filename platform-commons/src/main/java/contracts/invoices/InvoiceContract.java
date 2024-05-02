0971ae67e1c6dc1f53eaf7b87d5b4831d4e95817
ghita <ghita.cornel@gmail.com>
2024-05-02 20:36:25 +0300

    better

--- platform-commons/src/main/java/contracts/invoices/InvoiceContract.java
+++ platform-commons/src/main/java/contracts/invoices/InvoiceContract.java
@@ -3,33 +3,30 @@ package contracts.invoices;
 import commons.model.IdResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 public interface InvoiceContract {
 
     @PostMapping(value = "invoice", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     IdResponse create(@RequestBody InvoiceCreateRequest request);
 
-    @PostMapping(value = "invoice/update/order", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PatchMapping(value = "invoice/update/order", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void update(@RequestBody UpdateOrderRequest request);
 
-    @PostMapping(value = "invoice/update/client", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PatchMapping(value = "invoice/update/client", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void update(@RequestBody UpdateClientRequest request);
 
-    @PostMapping(value = "invoice/update/company", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PatchMapping(value = "invoice/update/company", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void update(@RequestBody UpdateCompanyRequest request);
 
-    @PostMapping(value = "invoice/update/product", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PatchMapping(value = "invoice/update/product", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void update(@RequestBody UpdateProductRequest request);
 
-    @PostMapping("invoice/complete/{id}")
+    @PatchMapping("invoice/complete/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void complete(@RequestBody @PathVariable("id") Integer id);
 
