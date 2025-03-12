d25d458dd231040a52c64d104ae4cfd0f6212563
cornel <ghita.cornel@gmail.com>
2025-03-12 14:05:37 +0200

    better

--- clients-app/src/main/java/clients/repository/ClientRepository.java
+++ clients-app/src/main/java/clients/repository/ClientRepository.java
@@ -4,6 +4,7 @@ import clients.repository.entity.Client;
 import clients.repository.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,7 +13,7 @@ public interface ClientRepository extends JpaRepository<Client, Integer> {
     List<Client> findByStatus(Status status);
 
     @Query("select c.status from Client c where c.id = :id")
-    Status findStatusById(int id);
+    Status findStatusById(@Param("id") int id);
 
     default List<Client> findAllActive() {
         return findByStatus(Status.ACTIVE);
--- companies-app/src/main/java/companies/repository/CompanyRepository.java
+++ companies-app/src/main/java/companies/repository/CompanyRepository.java
@@ -4,6 +4,7 @@ import companies.repository.entity.Company;
 import companies.repository.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,7 +13,7 @@ public interface CompanyRepository extends JpaRepository<Company, Integer> {
     List<Company> findByStatus(Status status);
 
     @Query("select c.status from Company c where c.id = :id")
-    Status findStatusById(int id);
+    Status findStatusById(@Param("id") int id);
 
     default List<Company> findAllActive() {
         return findByStatus(Status.ACTIVE);
