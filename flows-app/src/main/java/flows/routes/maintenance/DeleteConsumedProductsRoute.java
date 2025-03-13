47836258b2eed62fe0bdd3dbe717e882a6ed69f7
cornel <ghita.cornel@gmail.com>
2025-03-13 23:43:07 +0200

    better

--- flows-app/src/main/java/flows/routes/maintenance/DeleteConsumedProductsRoute.java
+++ flows-app/src/main/java/flows/routes/maintenance/DeleteConsumedProductsRoute.java
@@ -1,6 +1,7 @@
 package flows.routes.maintenance;
 
 import flows.clients.InvoiceClient;
+import flows.clients.OrderClient;
 import flows.clients.ProductClient;
 import lombok.RequiredArgsConstructor;
 import org.apache.camel.builder.RouteBuilder;
@@ -12,6 +13,7 @@ public class DeleteConsumedProductsRoute extends RouteBuilder {
 
     private final ProductClient productClient;
     private final InvoiceClient invoiceClient;
+    private final OrderClient orderClient;
 
     @Override
     public void configure() {
@@ -21,6 +23,11 @@ public class DeleteConsumedProductsRoute extends RouteBuilder {
                 .setBody(exchange -> productClient.findConsumedIds())
                 .split(body())
                 .parallelProcessing()
+                .setBody(exchange -> {
+                    Integer id = exchange.getMessage().getBody(Integer.class);
+                    return orderClient.existsByProductId(id) ? null : id;
+                })
+                .filter(body().isNotNull())
                 .setBody(exchange -> {
                     Integer id = exchange.getMessage().getBody(Integer.class);
                     return invoiceClient.existsByProductId(id) ? null : id;
