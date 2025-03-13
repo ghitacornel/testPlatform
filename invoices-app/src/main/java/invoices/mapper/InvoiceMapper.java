cd11df60979560aab1c7c8b3f535bf30f3253142
cornel <ghita.cornel@gmail.com>
2025-03-13 16:52:53 +0200

    better

--- invoices-app/src/main/java/invoices/mapper/InvoiceMapper.java
+++ invoices-app/src/main/java/invoices/mapper/InvoiceMapper.java
@@ -10,19 +10,58 @@ public interface InvoiceMapper {
     InvoiceDetails map(Invoice data);
 
     @Mapping(target = "id", ignore = true)
-    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    @Mapping(target = "clientName", ignore = true)
+    @Mapping(target = "clientCardType", ignore = true)
+    @Mapping(target = "clientCountry", ignore = true)
+    @Mapping(target = "productName", ignore = true)
+    @Mapping(target = "productColor", ignore = true)
+    @Mapping(target = "productPrice", ignore = true)
+    @Mapping(target = "companyId", ignore = true)
+    @Mapping(target = "companyName", ignore = true)
+    @Mapping(target = "companyUrl", ignore = true)
+    @Mapping(target = "companyIndustry", ignore = true)
+    @Mapping(target = "companyCountry", ignore = true)
+    @Mapping(target = "status", ignore = true)
     void update(@MappingTarget Invoice invoice, UpdateOrderRequest data);
 
     @Mapping(target = "id", ignore = true)
-    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    @Mapping(target = "orderQuantity", ignore = true)
+    @Mapping(target = "productId", ignore = true)
+    @Mapping(target = "productName", ignore = true)
+    @Mapping(target = "productColor", ignore = true)
+    @Mapping(target = "productPrice", ignore = true)
+    @Mapping(target = "companyId", ignore = true)
+    @Mapping(target = "companyName", ignore = true)
+    @Mapping(target = "companyUrl", ignore = true)
+    @Mapping(target = "companyIndustry", ignore = true)
+    @Mapping(target = "companyCountry", ignore = true)
+    @Mapping(target = "status", ignore = true)
     void update(@MappingTarget Invoice invoice, UpdateClientRequest data);
 
     @Mapping(target = "id", ignore = true)
-    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    @Mapping(target = "orderQuantity", ignore = true)
+    @Mapping(target = "clientId", ignore = true)
+    @Mapping(target = "clientName", ignore = true)
+    @Mapping(target = "clientCardType", ignore = true)
+    @Mapping(target = "clientCountry", ignore = true)
+    @Mapping(target = "productId", ignore = true)
+    @Mapping(target = "productName", ignore = true)
+    @Mapping(target = "productColor", ignore = true)
+    @Mapping(target = "productPrice", ignore = true)
+    @Mapping(target = "status", ignore = true)
     void update(@MappingTarget Invoice invoice, UpdateCompanyRequest data);
 
     @Mapping(target = "id", ignore = true)
-    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    @Mapping(target = "orderQuantity", ignore = true)
+    @Mapping(target = "clientId", ignore = true)
+    @Mapping(target = "clientName", ignore = true)
+    @Mapping(target = "clientCardType", ignore = true)
+    @Mapping(target = "clientCountry", ignore = true)
+    @Mapping(target = "companyName", ignore = true)
+    @Mapping(target = "companyUrl", ignore = true)
+    @Mapping(target = "companyIndustry", ignore = true)
+    @Mapping(target = "companyCountry", ignore = true)
+    @Mapping(target = "status", ignore = true)
     void update(@MappingTarget Invoice invoice, UpdateProductRequest data);
 
 }
