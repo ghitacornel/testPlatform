4caff188b85bc63d5a6814c314ad65b24e65f268
cornel <ghita.cornel@gmail.com>
2025-03-16 19:08:42 +0200

    better

--- flows-app/src/main/java/flows/routes/CompleteInvoiceRoute.java
+++ flows-app/src/main/java/flows/routes/CompleteInvoiceRoute.java
@@ -9,13 +9,11 @@ import feign.FeignException;
 import flows.clients.*;
 import flows.clients.InvoiceClient;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class CompleteInvoiceRoute extends RouteBuilder {
 
     private final InvoiceClient invoiceClient;
@@ -44,85 +42,57 @@ public class CompleteInvoiceRoute extends RouteBuilder {
                 })
                 .filter(body().isNotNull())
                 .process(exchange -> {
+
                     Integer id = exchange.getMessage().getBody(Integer.class);
-                    OrderDetailsResponse response;
+
                     try {
-                        response = orderClient.findById(id);
+
+
+                        OrderDetailsResponse orderDetailsResponse = orderClient.findById(id);
+                        invoiceClient.update(UpdateOrderRequest.builder()
+                                .id(id)
+                                .clientId(orderDetailsResponse.getClientId())
+                                .productId(orderDetailsResponse.getProductId())
+                                .orderQuantity(orderDetailsResponse.getQuantity())
+                                .build());
+
+
+                        ProductDetailsResponse productDetailsResponse = productClient.findById(orderDetailsResponse.getProductId());
+                        invoiceClient.update(UpdateProductRequest.builder()
+                                .id(id)
+                                .productId(productDetailsResponse.getId())
+                                .productName(productDetailsResponse.getName())
+                                .productColor(productDetailsResponse.getColor())
+                                .productPrice(productDetailsResponse.getPrice())
+                                .companyId(productDetailsResponse.getCompanyId())
+                                .build());
+
+                        ClientDetailsResponse clientDetailsResponse = clientClient.findById(orderDetailsResponse.getClientId());
+                        invoiceClient.update(UpdateClientRequest.builder()
+                                .id(id)
+                                .clientId(clientDetailsResponse.getId())
+                                .clientName(clientDetailsResponse.getName())
+                                .clientCardType(clientDetailsResponse.getCardType())
+                                .clientCountry(clientDetailsResponse.getCountry())
+                                .build());
+
+                        CompanyDetailsResponse companyDetailsResponse = companyClient.findById(productDetailsResponse.getCompanyId());
+                        invoiceClient.update(UpdateCompanyRequest.builder()
+                                .id(id)
+                                .companyId(companyDetailsResponse.getId())
+                                .companyName(companyDetailsResponse.getName())
+                                .companyUrl(companyDetailsResponse.getUrl())
+                                .companyIndustry(companyDetailsResponse.getIndustry())
+                                .companyCountry(companyDetailsResponse.getCountry())
+                                .build());
+
+                        invoiceClient.complete(exchange.getMessage().getBody(Integer.class));
+
                     } catch (FeignException e) {
                         log.error(e.getMessage());
-                        invoiceClient.error(id);
-                        return;
                     }
-                    invoiceClient.update(UpdateOrderRequest.builder()
-                            .id(id)
-                            .clientId(response.getClientId())
-                            .productId(response.getProductId())
-                            .orderQuantity(response.getQuantity())
-                            .build());
-                    exchange.getMessage().setHeader("clientId", response.getClientId());
-                    exchange.getMessage().setHeader("productId", response.getProductId());
+
                 })
-                .process(exchange -> {
-                    Integer id = exchange.getMessage().getBody(Integer.class);
-                    Integer productId = exchange.getMessage().getHeader("productId", Integer.class);
-                    ProductDetailsResponse response;
-                    try {
-                        response = productClient.findById(productId);
-                    } catch (FeignException e) {
-                        log.error(e.getMessage());
-                        invoiceClient.error(id);
-                        return;
-                    }
-                    invoiceClient.update(UpdateProductRequest.builder()
-                            .id(id)
-                            .productId(productId)
-                            .productName(response.getName())
-                            .productColor(response.getColor())
-                            .productPrice(response.getPrice())
-                            .companyId(response.getCompanyId())
-                            .build());
-                    exchange.getMessage().setHeader("companyId", response.getCompanyId());
-                })
-                .process(exchange -> {
-                    Integer id = exchange.getMessage().getBody(Integer.class);
-                    Integer clientId = exchange.getMessage().getHeader("clientId", Integer.class);
-                    ClientDetailsResponse response;
-                    try {
-                        response = clientClient.findById(clientId);
-                    } catch (FeignException e) {
-                        log.error(e.getMessage());
-                        invoiceClient.error(id);
-                        return;
-                    }
-                    invoiceClient.update(UpdateClientRequest.builder()
-                            .id(id)
-                            .clientId(clientId)
-                            .clientName(response.getName())
-                            .clientCardType(response.getCardType())
-                            .clientCountry(response.getCountry())
-                            .build());
-                })
-                .process(exchange -> {
-                    Integer id = exchange.getMessage().getBody(Integer.class);
-                    Integer companyId = exchange.getMessage().getHeader("companyId", Integer.class);
-                    CompanyDetailsResponse response;
-                    try {
-                        response = companyClient.findById(companyId);
-                    } catch (FeignException e) {
-                        log.error(e.getMessage());
-                        invoiceClient.error(id);
-                        return;
-                    }
-                    invoiceClient.update(UpdateCompanyRequest.builder()
-                            .id(id)
-                            .companyId(companyId)
-                            .companyName(response.getName())
-                            .companyUrl(response.getUrl())
-                            .companyIndustry(response.getIndustry())
-                            .companyCountry(response.getCountry())
-                            .build());
-                })
-                .process(exchange -> invoiceClient.complete(exchange.getMessage().getBody(Integer.class)))
                 .end();
     }
 
