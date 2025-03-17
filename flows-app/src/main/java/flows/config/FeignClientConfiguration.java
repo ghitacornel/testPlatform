9e10743b9f933383f58ff23e46df8fbe4b93fb29
cornel <ghita.cornel@gmail.com>
2025-03-17 12:52:16 +0200

    better

--- flows-app/src/main/java/flows/config/FeignClientConfiguration.java
+++ flows-app/src/main/java/flows/config/FeignClientConfiguration.java
@@ -1,10 +1,12 @@
 package flows.config;
 
 import contracts.clients.exceptions.ExceptionErrorDecoder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableFeignClients
 class FeignClientConfiguration {
 
     @Bean
