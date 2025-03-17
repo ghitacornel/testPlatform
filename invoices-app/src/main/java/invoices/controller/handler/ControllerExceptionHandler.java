5932d6b175345ce7d3f031e3568783258cc91527
cornel <ghita.cornel@gmail.com>
2025-03-17 22:35:03 +0200

    better

--- invoices-app/src/main/java/invoices/controller/handler/ControllerExceptionHandler.java
+++ invoices-app/src/main/java/invoices/controller/handler/ControllerExceptionHandler.java
@@ -4,13 +4,11 @@ import commons.exceptions.CommonControllerExceptionHandler;
 import commons.exceptions.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
 @RestControllerAdvice
