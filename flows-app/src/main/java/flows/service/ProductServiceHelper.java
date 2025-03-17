ed00fbd901383f3435fa0cd5b50f5e48b17092e8
cornel <ghita.cornel@gmail.com>
2025-03-17 22:16:04 +0200

    better

--- flows-app/src/main/java/flows/service/ProductServiceHelper.java
+++ flows-app/src/main/java/flows/service/ProductServiceHelper.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Slf4j
 @Component
