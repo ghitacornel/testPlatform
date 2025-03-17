2073a0f391b1be21b1195edd821e5ed7e339bed6
cornel <ghita.cornel@gmail.com>
2025-03-17 07:33:42 +0200

    better

--- orders-app/src/main/java/orders/service/RemoveOrdersSchedulerService.java
+++ orders-app/src/main/java/orders/service/RemoveOrdersSchedulerService.java
@@ -1,13 +1,13 @@
 package orders.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import orders.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class RemoveOrdersSchedulerService {
 
     private final OrderRepository repository;
