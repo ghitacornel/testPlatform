0b4bf33cd18758fac0fe59c077e14f1a724d8de4
cornel <ghita.cornel@gmail.com>
2025-03-17 22:34:47 +0200

    better

--- invoices-app/src/main/java/invoices/controller/handler/ControllerExceptionHandler.java
+++ invoices-app/src/main/java/invoices/controller/handler/ControllerExceptionHandler.java
@@ -1,8 +1,25 @@
 package invoices.controller.handler;
 
 import commons.exceptions.CommonControllerExceptionHandler;
+import commons.exceptions.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Slf4j
 @RestControllerAdvice
 public class ControllerExceptionHandler extends CommonControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        log.error("data integrity {}", e.getMessage());
+        return new ResponseEntity<>(new ErrorResponse(e.getMessage()), BAD_REQUEST);
+    }
+
 }
