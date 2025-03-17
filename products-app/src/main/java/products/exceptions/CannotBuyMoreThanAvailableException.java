7d5c66ed9ae71527ca7b70de516ecadb039aba14
cornel <ghita.cornel@gmail.com>
2025-03-17 19:47:46 +0200

    better

--- products-app/src/main/java/products/exceptions/CannotBuyMoreThanAvailableException.java
+++ products-app/src/main/java/products/exceptions/CannotBuyMoreThanAvailableException.java
@@ -5,6 +5,6 @@ import commons.exceptions.BusinessException;
 public class CannotBuyMoreThanAvailableException extends BusinessException {
 
     public CannotBuyMoreThanAvailableException(int productId, int requested, int available) {
-        super("Cannot buy more that available product id" + productId + ", requested " + requested + ", available " + available);
+        super("Cannot buy more that available product id " + productId + ", requested " + requested + ", available " + available);
     }
 }
