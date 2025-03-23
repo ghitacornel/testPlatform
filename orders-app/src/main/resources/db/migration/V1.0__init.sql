eef7184319a0f953e594e4af58829bd9c5300a17
cornel <ghita.cornel@gmail.com>
2025-03-23 14:42:10 +0200

    better

--- orders-app/src/main/resources/db/migration/V1.0__init.sql
+++ orders-app/src/main/resources/db/migration/V1.0__init.sql
@@ -7,12 +7,3 @@ create table ORDERS
     status        smallint not null,
     reject_reason varchar(255)
 );
-create table ORDERS_ARCHIVE
-(
-    id            int primary key,
-    client_id     int      not null,
-    product_id    int      not null,
-    quantity      int      not null,
-    status        smallint not null,
-    reject_reason varchar(255)
-);
