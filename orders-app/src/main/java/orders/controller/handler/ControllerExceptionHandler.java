d6b2d31ddd2ba024605d54975b9dbbc6b4590558
cornel <ghita.cornel@gmail.com>
2025-03-23 13:41:14 +0200

    better

--- orders-app/src/main/java/orders/controller/handler/ControllerExceptionHandler.java
+++ orders-app/src/main/java/orders/controller/handler/ControllerExceptionHandler.java
@@ -3,12 +3,10 @@ package orders.controller.handler;
 import commons.exceptions.CommonControllerExceptionHandler;
 import commons.exceptions.ErrorResponse;
 import orders.exceptions.OrderNotFoundException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
