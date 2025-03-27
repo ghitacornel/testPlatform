832fc9e69c47e0da0cbb9714a2bc261632d9668a
cornel <ghita.cornel@gmail.com>
2025-03-27 14:30:01 +0200

    better

--- platform-commons/src/main/java/contracts/flows/FlowsContract.java
+++ platform-commons/src/main/java/contracts/flows/FlowsContract.java
@@ -8,18 +8,18 @@ import org.springframework.web.bind.annotation.*;
 
 public interface FlowsContract {
 
-    @PostMapping(value = "camel/orders/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "flow/orders/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     IdResponse createOrder(@RequestBody CreateOrderRequest inputModel);
 
-    @PatchMapping("camel/orders/cancel/{id}")
+    @PatchMapping("flow/orders/cancel/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void cancelOrder(@PathVariable("id") Integer id);
 
-    @DeleteMapping("camel/companies/delete/{id}")
+    @DeleteMapping("flow/companies/delete/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void deleteCompany(@PathVariable("id") Integer id);
 
-    @DeleteMapping("camel/clients/delete/{id}")
+    @DeleteMapping("flow/clients/delete/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void deleteClient(@PathVariable("id") Integer id);
 
