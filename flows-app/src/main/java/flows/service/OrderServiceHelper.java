2d98448b109c5a97059636a258825fbd61c1b3b9
cornel <ghita.cornel@gmail.com>
2025-03-27 23:21:07 +0200

    better

--- flows-app/src/main/java/flows/service/OrderServiceHelper.java
+++ flows-app/src/main/java/flows/service/OrderServiceHelper.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -19,6 +20,7 @@ class OrderServiceHelper {
     private final OrderClient orderClient;
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    @Async
     void sendCompletedToInvoice(Integer id) {
         kafkaTemplate.send(completedOrdersTopic, String.valueOf(id));
 
