144144d9c293ca5aa328ff70eb3f4ba1e50d3379
cornel <ghita.cornel@gmail.com>
2025-03-27 22:47:13 +0200

    better

--- orders-app/src/main/java/orders/service/OrdersSchedulerService.java
+++ orders-app/src/main/java/orders/service/OrdersSchedulerService.java
@@ -4,7 +4,6 @@ import contracts.orders.Status;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import orders.repository.OrderRepository;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,17 +13,14 @@ public class OrdersSchedulerService {
 
     private final OrderRepository repository;
 
-    @Async
     public void removeCancelledOrders() {
         repository.deleteByStatus(Status.CANCELLED);
     }
 
-    @Async
     public void removeRejectedOrders() {
         repository.deleteByStatus(Status.REJECTED);
     }
 
-    @Async
     public void removeInvoicedOrders() {
         repository.deleteByStatus(Status.INVOICED);
     }
