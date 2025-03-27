c0c5026fd347c449da5d28d013b0aa96aa2e3578
cornel <ghita.cornel@gmail.com>
2025-03-27 14:21:50 +0200

    better

--- simulator/src/main/java/platform/Simulator.java
+++ simulator/src/main/java/platform/Simulator.java
@@ -3,11 +3,7 @@ package platform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAsync
-@EnableScheduling
 @EnableFeignClients
 @SpringBootApplication
 public class Simulator {
