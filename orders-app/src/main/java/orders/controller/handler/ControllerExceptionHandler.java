94625747119a96a526f5c42b9dd3f7981dac0901
cornel <ghita.cornel@gmail.com>
2025-03-29 21:31:12 +0200

    better

--- orders-app/src/main/java/orders/controller/handler/ControllerExceptionHandler.java
+++ orders-app/src/main/java/orders/controller/handler/ControllerExceptionHandler.java
@@ -1,8 +1,22 @@
 package orders.controller.handler;
 
 import commons.exceptions.CommonControllerExceptionHandler;
+import commons.exceptions.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@Slf4j
 @RestControllerAdvice
 public class ControllerExceptionHandler extends CommonControllerExceptionHandler {
+
+    @ExceptionHandler(org.hibernate.StaleObjectStateException.class)
+    public ResponseEntity<Object> handleStaleObjectStateException(org.hibernate.StaleObjectStateException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(new ErrorResponse(e.getMessage()), INTERNAL_SERVER_ERROR);
+    }
+
 }
